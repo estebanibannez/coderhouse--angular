b5cbd43eef44275c4fa8e5ef4a6209e772c868ca
Esteban Ibañez <e.ibannez.p@gmail.com>
2021-11-18 22:27:14 -0300

    se crea estructura base del proyecto

---
+++ clase-04/proyectoCuatro/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TestComponent } from './components/test/test.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './shared/home/home.component';
+import { CardComponent } from './components/card/card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    CardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
